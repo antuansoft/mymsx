8682f390280d81ab3f1f2970cfa38106ec1fcad9
antuansoft <amartinezalf@yahoo.es>
2020-10-24 23:23:17 +0200

    arkanoid game added

--- js/games_data.js
+++ js/games_data.js
@@ -12,9 +12,10 @@ var FreddyHardest = {
                    Juegazo muy adictivo para el año que era. Pero todavía hoy merece pasarselo entero de nuevo. <br> <br>\
                    Por cierto la música super pegadiza, también te vas a enganchar a ella.",
     tricks: "La clave para la segunda parte es 897653",
-    game_mobile: "Espera que cargue el juego, usa espacio del teclado para llegar la opción de redefinir, \
-                  Una vez seleccionado redefine pulsando en el pad \
-                  las distintas direcciones y el disparo. Una vez acabado pulsa 1 en el teclado para jugar.",
+    game_mobile: "Espera que cargue el juego, usa la opción de teclado para llegar al botón de espacio del teclado y elegir \
+                  la opción de redefinir. Una vez seleccionado redefine pulsando en el pad \
+                  las distintas direcciones y el disparo. Una vez acabado pulsa 1 en el teclado para jugar. </br></br> \
+                  Nota: Si pones el movil en horizontal tendrás la mejor vista para jugar.",
     mobile_pad: "grey_pad.JPG",
     controls: " <tr> \
                   <th>Arriba</th> \
@@ -46,8 +47,44 @@ var Buitre = {
     si haces una falta dentro del area no es PENALTY, y sólo son 15 minutos de partido, pero el resto del juego merece la pena. \
     <br> Es un juego simple sin segunda parte, sin cambios de campo pero fue un buen trabajo y nos tuvo mucho tiempo entretenidos. \
     Nosotros jugamamos partido de ida en mi casa con el MSX y partido de vuelta en su casa con el AMSTRAD. Que recuerdos.",
-    tricks: "Si se te hace fácil busca la opción de poner la velocidad del emulador en 2x (quick options --> Z80 CPU Clock) y te aseguro que no será fácil ganar",
-    game_mobile: "Espera que cargue el juego, usa espacio del teclado para llegar la opción de redefinir, despues pulsa en la rueda dentada \
+    tricks: "Si se te hace fácil busca la opción de poner la velocidad del emulador en 2x (rueda dentada --> quick options --> Z80 CPU Clock) y te aseguro que no será fácil ganar.",
+    game_mobile: "Espera que cargue el juego, usa la opción de teclado para llegar al botón de espacio del teclado y elegir la opción de redefinir, despues pulsa en la rueda dentada \
+                  busca opción de Touch Setup y cambia el pad al que tiene las teclas moradas. Una vez seleccionado redefine pulsando en el pad \
+                  las distintas direcciones y el disparo. Una vez acabado pulsa cero en el teclado para jugar.",
+    mobile_pad: "purple_pad.JPG",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Fuego</th>\
+                  <th>Pausa</th> \
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>Q</td> \
+                  <td>A</td> \
+                  <td>P</td> \
+                  <td>O</td> \
+                  <td>Espacio</td>\
+                  <td></td>\
+                  <td>SI</td>\
+                </tr> "
+  };
+
+var Arkanoid = {
+    game_name: "Arkanoid",
+    breadcrumb_name: "Arkanoid",
+    game_player_url: "player.html?DISK=games/Arkanoid.dsk&SCREEN_FULLSCREEN_MODE=1",
+    description: " Yo creo que este fue uno de los primeros juegos a los que jugué cuando me trajeron mi MSX. \
+    es un juegazo de 1986 de Taito, marca icónica en las recreativas. Mi padre llamaba a este juego el de los ladrillos \
+    y era el único que le gustaba en el ordenador. Es un juego muy sencillo que consiste en romper todo ladrillo que veas \
+    en la pantalla. Cada nivel es distinto con disposición distinta de la pantalla, los ladrillos y los tipos de ladrillo \
+    y solo quieres pasar a la siguiente pantalla, pero......a veces te matan antes y hay que empezar de nuevo.",
+    tricks: "Como en el emulador puedes guardar la partida espero que llegues muy lejos. Tus aliados con los ladrillos \
+    el de colo ROJO te permite disparar, el GRIS es vida extra y el MORADO CLARO te abre un pasadizo lateral al sigueinte nivel,\
+    y el AMARILLO hará que la bola sea más lenta, esos son los más poderosos úsalos adecuadamente.",
+    game_mobile: "Espera que cargue el juego, usa la opción de teclado para llegar al botón de espacio del teclado y elegir la opción de redefinir, despues pulsa en la rueda dentada \
                   busca opción de Touch Setup y cambia el pad al que tiene las teclas moradas. Una vez seleccionado redefine pulsando en el pad \
                   las distintas direcciones y el disparo. Una vez acabado pulsa cero en el teclado para jugar.",
     mobile_pad: "purple_pad.JPG",
