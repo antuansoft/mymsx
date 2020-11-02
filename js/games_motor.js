e0e80606c5bd8d703711b1575997164983da859b
antuansoft <amartinezalf@yahoo.es>
2020-11-02 11:25:31 +0100

    motor section images

--- js/games_motor.js
+++ js/games_motor.js
@@ -3,14 +3,13 @@ var F1Spirit = {
     game_name: "F1 Spirit",
     breadcrumb_name: "F1Spirit",
     game_player_url: "player.html?DISK=games/F1Spirit.dsk&SCREEN_FULLSCREEN_MODE=1",
-    description: "Creo que sin duda es uno de los mejores juegos de coches para MSX, pues pilotar un coche de rally, un prototipo, un F3 y hasta un F1.\
+    description: "Creo que sin duda es uno de los mejores juegos de coches para MSX, pues pilotar un coche de rally,  prototipos, resistencia, un F3 y hasta un F1.\
     </br> Tiene para mi una opción que ninguno tenía que es la opción de parar en boxes a repostar y reparar el coche.\
-    </br> Al cargar el juego seleciona la opción 1 que te da acceso a todos los circuitos y modalidades, la opción 0 es el juego normal.\
-    ",
+    </br> Al cargar el juego seleciona la opción 1 que te da acceso a todos los circuitos y modalidades, la opción 0 es el juego normal.",
     tricks: "Para parar en boxes tienes que entrar en el carril de boxes y frenar para que el coche quede parado en uno de los \
     slots de boxes. OJO que si tienes mal los frenos hay que empezar a frenar muchisimo antes, sino te saltas el box. \
     Esto hay que entrenarlo en todas las modalidades porque es distinto y vigila la gasolina que necesitas para llegar al final \
-    ten en cuenta que casi siempre va a tener que hacer mínimo dos paradas en boxes.",
+    ten en cuenta que casi siempre vas a tener que hacer mínimo dos paradas en boxes.",
     game_mobile: "Desde la opción del teclado tendrás que seleccionar el juego normal (0) o con todas las opciones habilitadas (1) \
     Despues en la rueda dentada, busca opción de Touch Setup y configura dos teclas para que el SPC y la tecla N aparezcan, el pad ya está configurado.",
     mobile_pad: "grey_pad.JPG",
