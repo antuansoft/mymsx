dca3a0073cc8b83335bcbc28401bfa1fff0e5745
Antuansoft <amartinezalf@yahoo.es>
2020-12-13 01:18:46 +0100

    Dunkshot game added

---
+++ js/games_sports.js
@@ -0,0 +1,42 @@
+var images_folder= "images"
+var breadcrumb_level1 = "<a href='sports.html'>/deportes</a>";
+var Dunkshot = {
+    game_name: "Dunkshot",
+    breadcrumb_name: "Dunkshot",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?ROM=games/Dunkshot.rom&SCREEN_FULLSCREEN_MODE=1",
+    description: "Para mi el mejor juego de baloncesto de 8 bits y un adelantado a su tiempo. \
+    Es un juego donde puedes EDITAR los nombre de los jugadores, su aspecto, la cancha de juego, vender jugadores y \
+    hacer que el ordenado juege con el ordenador, visualizando un partido.</br> \
+    El juego consiste e ir jugando partidos e ir ganando experiencia hasta subir de nivel a tus jugadores y ganar a los mejores.</br>\
+    Son solo 3 jugadores por las limitaciones de los sprites en pantalla, por eso parpadea mucho al superponerse unos y otros \
+    pero le juego es super adictivo y como digo un adelantado a su tiempo.",
+    tricks: "Para tirar a canasta tienes que dejar el disparo pulsado, el jugador salta y el al soltar el disparo se produce el tiro a canasta.</br>\
+    Los jugadores se cansan, tienes que hacer cambios y dar experienca a todos tus 8 jugadores.\
+    En cuanto haya una interrupción en el partido pulsa espacio y haz cambios con jugadores más frescos.</br>\
+    Al principio cuesta que los jugadores encesten incluso estando al lado de la canasta, sólo ganando partidos irán progresando.\
+    así que juega partidos con rivales de tu experiencia y también puedes intercambiar jugadores con la opción de trade.",
+    game_mobile: "",
+    mobile_pad: "grey_pad.JPG",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Tirar/Pasar</th>\
+                  <th>Cambio de jugador/cambio de formación</th> \
+                  <th>Cambios</th> \
+                  <th>Salir</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>] (tecla corchete cerrado al lado del Enter)</td>\
+                  <td>Enter</td>\
+                  <td>Espacio</td>\
+                  <td>ESC</td>\
+                </tr> ",
+  };
+
