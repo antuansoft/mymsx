7c7d7f357f586ebb1ca0583798dc78b75dab14de
Antuansoft <amartinezalf@yahoo.es>
2020-12-27 00:25:38 +0100

    New game Chess 2 Added

---
+++ js/games_strategy.js
@@ -0,0 +1,31 @@
+var images_folder= "images"
+var breadcrumb_level1 = "<a href='strategy.html'>/estrategia</a>";
+var Chessgame2 = {
+    game_name: "The Chess Game",
+    breadcrumb_name: "The Chess Game",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?DISK=games/Chessgame2.dsk&SCREEN_FULLSCREEN_MODE=1",
+    description: "Clásico juego de ajedrez en formato 3D que lo hace más vistoso, y además con unas opciones en el menú donde te permite \
+    crear un partida con las fichas deseadas y por supuesto empezar una partida desde cero. Una versión de ajedrez muy completa.\
+    Quizas lo que hecho en falta son niveles de dificultad pero para el nivel básico es perfecto.",
+    tricks: "",
+    game_mobile: "No hay que configurar nada, funciona con las flechas de pad y el botón A para seleccionar.",
+    mobile_pad: "grey_pad.JPG",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Selecci&oacute;n</th>\
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>Espacio</td>\
+                  <td>NO</td>\
+                </tr> ",
+};
+
