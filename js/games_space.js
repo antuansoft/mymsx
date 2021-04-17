73b382dadaaad565044da1d8602db7fa2f91e96a
Antuansoft <amartinezalf@yahoo.es>
2021-04-17 10:00:34 +0200

    Sky jaguar game added

---
+++ js/games_space.js
@@ -0,0 +1,33 @@
+var images_folder= "images"
+var breadcrumb_level1 = "<a href='space.html'>/Naves</a>";
+var SkyJaguar = {
+    game_name: "Sky Jaguar",
+    breadcrumb_name: "Sky Jaguar",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?ROM=games/SkyJaguar.rom&SCREEN_FULLSCREEN_MODE=1",
+    description: "El primer juego de naves al que jugué con el msx, es un juego de 1984 donde tenemos que pasar distintos niveles \
+                  y destruir a los distintos enemigos que nos atacan.",
+    tricks: "Al matar enemigos aparecerá un objeto POW para cogerlo, eso incrementará tu capacidad de disparo y es vital para \
+             lograr terminar el juego. Pero lo mejor es ser hábil con los controlos para escribar los multiples disparos.",
+    game_mobile: "Espera que cargue el juego, pulsa en la rueda dentada \
+    busca opción de Touch Setup y cambia un par de botones por SHP y SPC, sal de la configuración, pulsa SHP y a jugar.",
+    mobile_pad: "purple_pad.JPG",
+    video:"https://www.youtube.com/embed/pKyqZzgJ46c",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Disparo</th>\
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>espacio</td>\
+                  <td>NO</td>\
+                </tr> ",
+  };
+
