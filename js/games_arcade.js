1ab5743682400766c96e599b0b6cabcdf00dd5e3
Antuansoft <amartinezalf@yahoo.es>
2021-04-24 01:25:51 +0200

    outrun added

---
+++ js/games_arcade.js
@@ -0,0 +1,31 @@
+var images_folder= "images"
+var breadcrumb_level1 = "<a href='arcade.html'>/Recreativas</a>";
+var OutRun = {
+    game_name: "Out Run",
+    breadcrumb_name: "Out Run",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?ROM=games/OutRun.rom&SCREEN_FULLSCREEN_MODE=1",
+    description: "Es una version del clásico de la recreativa de 1988 hecho por Pony Canyon, no está nada mal la música, lo graficos flojean pero no es un mal \
+                  simulador de la recreativa original",
+    tricks: "No te emociones demasiado el truco está en no chocar, así que en las curvas mucho cuidado. Si vuelvas el coche date por perdido.",
+    game_mobile: "Listo para jugar en movil.",
+    mobile_pad: "grey_pad.JPG",
+    video:"https://www.youtube.com/embed/pVr-HfKHbjI?start=50",
+    controls: " <tr> \
+                  <th>Marcha larga</th> \
+                  <th>Frenar/Marcha corta</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Acelerar</th>\
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>espacio</td>\
+                  <td>NO</td>\
+                </tr> ",
+  };
+
