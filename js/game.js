004cc2bdcd79bec0bd92831f46ab6c44d9abaa73
Antuansoft <amartinezalf@yahoo.es>
2021-05-12 15:00:18 +0200

    Added tests sections

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
+var images_folder= "images"
+var breadcrumb_level1 = "<a href='pruebas.html'>/Pruebas</a>";
+var pruebas = {
+    game_name: "Pruebas",
+    breadcrumb_name: "Pruebas",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?SCREEN_FULLSCREEN_MODE=1",
+    description: "Carga básica del sistemas operativo MSX para pruebas.",
+    tricks: "Puedas cargar tus cassetes, disk o rom con los controles desde tu propio ordenador.",
+    game_mobile: "La misa funcionalidad desde el movil",
+    mobile_pad: "grey_pad.JPG",
+}
     //read input parameters
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
