482c44c2e188b961961210beb047e0d8099bc1b4
Antuansoft <amartinezalf@yahoo.es>
2021-05-31 20:01:54 +0200

    Galaxian added

--- js/games_space.js
+++ js/games_space.js
@@ -10,7 +10,7 @@ var SkyJaguar = {
     tricks: "Al matar enemigos aparecerá un objeto POW para cogerlo, eso incrementará tu capacidad de disparo y es vital para \
              lograr terminar el juego. Pero lo mejor es ser hábil con los controlos para escribar los multiples disparos.",
     game_mobile: "Juego configurado y listo para jugar",
-    mobile_pad: "purple_pad.JPG",
+    mobile_pad: "grey_pad.JPG",
     video:"https://www.youtube.com/embed/pKyqZzgJ46c",
     controls: " <tr> \
                   <th>Arriba</th> \
@@ -30,3 +30,33 @@ var SkyJaguar = {
                 </tr> ",
   };
 
+  var Galaxian = {
+    game_name: "Galaxian",
+    breadcrumb_name: "Galaxian",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?ROM=games/Galaxian.rom&SCREEN_FULLSCREEN_MODE=1",
+    description: "Uno de los primeros que me llamó la antención en las recreativas, es un juego de 1979 de NAMCO, traido al formato de 8 bits por NAMCOT \
+                  la compañia creada para el formato de casa. Es un clásico de las recreativas \
+                  donde siempre es igual, una orda de naves que nos atacan y tenemos que destruirlas a todas. \
+                  Hay que tener cuidado con los amarillos y rojos que pueden atacar en grupos de 3 naves.",
+    tricks: "Si destruyes al grupo de 3 naves que te atacan primero a los rojos y luego al amarillo optendrás 800 puntos.",
+    game_mobile: "Juego configurado y listo para jugar",
+    mobile_pad: "grey_pad.JPG",
+    video:"https://www.youtube.com/embed/Z3Udn2eBZcQ?start=42",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Disparo</th>\
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>espacio</td>\
+                  <td>NO</td>\
+                </tr> ",
+  };
