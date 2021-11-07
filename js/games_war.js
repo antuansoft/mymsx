3794c9b456dd88b2881a40dd2ff9f9e9eb95b0fe
Antuansoft <amartinezalf@yahoo.es>
2021-11-07 23:34:00 +0100

    1942 game added

--- js/games_war.js
+++ js/games_war.js
@@ -33,3 +33,38 @@ var GreenBeret = {
                 </tr> ",
   };
 
+
+  var g1942 = {
+    game_name: "1942",
+    breadcrumb_name: "1942",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?DISK=games/1942.dsk&SCREEN_FULLSCREEN_MODE=1",
+    description: "Estamos en 1942 la batalla por el control aereo en la segunda guerra mundial está servida. Tienes que defender tu territorio \
+                  por 32 escenarios distintos. Para ello cuentas con tu caza sus disparos y la opción de hacer un 360º cuando te veas en peligro.\
+                  Gracisima conversión a 8 bits de la recrativa super conocida creado en 1986 por ASCII que nos permitió jugar a unos de los favoritos\
+                  de las recreativas de la época en nuestro MSX.",
+    tricks: "Usa el giro 360 para librar las situaciones apuradas y moverte a otro lado de la pantalla libre de enemigos.",
+    game_mobile: "Espera que cargue el juego, pulsa en la rueda dentada \
+                  busca opción de Touch Setup y cambia un botón por la tecla Shift.",
+    mobile_pad: "grey_pad.JPG",
+    video:"https://www.youtube.com/embed/dXarj-ZWvXM",
+    controls: " <tr> \
+                  <th>Arriba</th> \
+                  <th>Abajo</th> \
+                  <th>Derecha</th> \
+                  <th>Izquierda</th> \
+                  <th>Disparo</th>\
+                  <th>Giro 360º</th>\
+                  <th>Redefine</th> \
+                </tr> \
+                <tr> \
+                  <td>&uarr;</td> \
+                  <td>&darr;</td> \
+                  <td>&rarr;</td> \
+                  <td>&larr;</td> \
+                  <td>spacio</td>\
+                  <td>shift</td>\
+                  <td>NO</td>\
+                </tr> ",
+  };
+
