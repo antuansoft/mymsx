bca5b2ce554370a1d80be13d0ef56d624a0723b9
Antuansoft <amartinezalf@yahoo.es>
2022-01-17 23:26:20 +0100

    Aleste game

--- js/games_space.js
+++ js/games_space.js
@@ -1,5 +1,41 @@
 var images_folder= "images"
 var breadcrumb_level1 = "<a href='space.html'>/Naves</a>";
+var Aleste = {
+  game_name: "Aleste",
+  breadcrumb_name: "Aleste",
+  breadcrumb_level1: breadcrumb_level1,
+  game_player_url: "player.html?DISK=games/Aleste.dsk&SCREEN_FULLSCREEN_MODE=1",
+  description: "Uno de los mejores juegos de Naves para MSX. De la super compañia especializada en este genero Compile.\
+                Esta versión es una 'mejora' de Zanac ya que este es para MSX2 y podéis ver las diferencias con el Zanac. </br>\
+                Para mi es un juego super dificil y es super largo y no soy muy bueno en estos de naves pero el vídeo del maestro\
+                Araubi os lo explica todo.",
+  tricks: "En el juego cuentas con 2 armas la principal que se va alimentado con las 'P' que nos van lloviendo y las armas \
+           especiales que son números que iremos descubriendo disparando a todo lo que se mueve, estas son las  que son\
+           las realmente potentes y puede elegir el mismo número hasta 3 veces que la hará cada vez más potente.<br>\
+           Los enemigos finales van por tiempo como en Zanac, tienes un tiempo limitado para poder destruirlos.</br>\
+           Otro aspecto importante es que puesdes destruir los disparos enemigos con tu propio disparo, cuando lluevan muchas \
+           balas es la mejor opción. <br> Por ultimo destacar que al coger las 'P' tendrás medio segundo de inmmunidad aprovecha bien.",
+  game_mobile: "Juego configurado y listo para jugar",
+  mobile_pad: "grey_pad.JPG",
+  video:"https://www.youtube.com/embed/7TVdztVLBFQ?start=50",
+  controls: " <tr> \
+                <th>Arriba</th> \
+                <th>Abajo</th> \
+                <th>Derecha</th> \
+                <th>Izquierda</th> \
+                <th>Disparo</th>\
+                <th>Redefine</th> \
+              </tr> \
+              <tr> \
+                <td>&uarr;</td> \
+                <td>&darr;</td> \
+                <td>&rarr;</td> \
+                <td>&larr;</td> \
+                <td>espacio</td>\
+                <td>NO</td>\
+              </tr> ",
+};
+
 var SkyJaguar = {
     game_name: "Sky Jaguar",
     breadcrumb_name: "Sky Jaguar",
